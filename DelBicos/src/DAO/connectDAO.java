c78522ca8cdb3d21d7152a76d1df62ec99bf69c6
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2025-06-17 19:57:55 -0300

    feat(connectDAO): implement database connection and CRUD operations

---
+++ DelBicos/src/DAO/connectDAO.java
@@ -0,0 +1,193 @@
+/*
+* Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+* Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+*/
+package DAO;
+ 
+ 
+import java.util.List;
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+// import javax.swing.table.AbstractTableModel;
+ 
+/**
+*
+ * @author Alunos
+*/
+public class connectDAO {
+    
+    Connection conn;
+    
+    public Connection connectDB(){
+        JOptionPane.showMessageDialog(null, "Inicia a classe para conexão com SQL SERVER!");
+ 
+        String caminho = "jdbc:sqlserver://localhost:1433;databaseName=DEL_BICOS;"
+                + "encrypt=true;trustServerCertificate=true;"; 
+        String usuario = "sa";
+        String senha = ".";
+        try {
+            conn = DriverManager.getConnection(caminho, usuario, senha);
+            JOptionPane.showMessageDialog(null, "Conectado com sucesso!");
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro de conexão, connectDAO - Mensagem => "+erro.getMessage());
+            JOptionPane.showMessageDialog(null, "\n Erro de conexão, connectDAO - Estado => "+erro.getSQLState());
+            JOptionPane.showMessageDialog(null, "\n Erro de conexão, connectDAO - Código => "+erro.getErrorCode());
+        }
+        return conn;
+        // conn.close();
+    }
+    
+    /*
+    * Rotina para inserir um registro novo no banco de dados
+    */
+    public void insereRegistroJFBD(BaseDAO obj) {
+        String tabela = obj.getTableName();
+        String strDados = obj.dadosSQLValues();
+        conn = connectDB();
+        Statement stmt;
+        try {
+            stmt = conn.createStatement();
+            
+            String sql = "INSERT INTO dbo."+tabela+" VALUES ("+strDados+")";
+            JOptionPane.showMessageDialog(null, "String de insert: "+sql);
+            
+            try {
+                stmt.executeUpdate(sql);
+                JOptionPane.showMessageDialog(null, "Inclusão executada com sucesso!");
+            } catch (SQLException erro) {
+                JOptionPane.showMessageDialog(null, "Erro de conexão, connectDAO - Mensagem => "+erro.getMessage());
+                JOptionPane.showMessageDialog(null, "\n Erro de conexão, connectDAO - Estado => "+erro.getSQLState());
+                JOptionPane.showMessageDialog(null, "\n Erro de conexão, connectDAO - Código => "+erro.getErrorCode());
+            }
+            conn.close();
+        } catch (SQLException erro) {
+            Logger.getLogger(connectDAO.class.getName()).log(Level.SEVERE, null, erro);
+        }
+    }
+
+    public void alteraRegistroJFBD(BaseDAO obj) {
+        String tabela = obj.getTableName();
+        String strDados = obj.alteraDadosSQLValues();
+        String condicao = obj.termoSQLWhereById();
+        conn = connectDB();
+        Statement stmt;
+        try {
+            stmt = conn.createStatement();
+            
+            String sql = "UPDATE dbo."+tabela+" SET "+strDados+" WHERE "+condicao;
+            JOptionPane.showMessageDialog(null, "String de update: "+sql);
+            
+            try {
+                stmt.executeUpdate(sql);
+                JOptionPane.showMessageDialog(null, "Alteração executada com sucesso!");
+            } catch (SQLException erro) {
+                JOptionPane.showMessageDialog(null, "Erro de conexão, connectDAO - Mensagem => "+erro.getMessage());
+                JOptionPane.showMessageDialog(null, "\n Erro de conexão, connectDAO - Estado => "+erro.getSQLState());
+                JOptionPane.showMessageDialog(null, "\n Erro de conexão, connectDAO - Código => "+erro.getErrorCode());
+            }
+            conn.close();
+        } catch (SQLException erro) {
+            Logger.getLogger(connectDAO.class.getName()).log(Level.SEVERE, null, erro);
+        }
+    }
+
+    public void excluiRegistroJFBD(BaseDAO obj) {
+        String tabela = obj.getTableName();
+        String condicao = obj.termoSQLWhereById();
+        conn = connectDB();
+        Statement stmt;
+        try {
+            stmt = conn.createStatement();
+            
+            String sql = "DELETE FROM dbo."+tabela+" WHERE "+condicao;
+            JOptionPane.showMessageDialog(null, "String de delete: "+sql);
+            
+            try {
+                stmt.executeUpdate(sql);
+                JOptionPane.showMessageDialog(null, "Exclusão executada com sucesso!");
+            } catch (SQLException erro) {
+                JOptionPane.showMessageDialog(null, "Erro de conexão, connectDAO - Mensagem => "+erro.getMessage());
+                JOptionPane.showMessageDialog(null, "\n Erro de conexão, connectDAO - Estado => "+erro.getSQLState());
+                JOptionPane.showMessageDialog(null, "\n Erro de conexão, connectDAO - Código => "+erro.getErrorCode());
+            }
+            conn.close();
+        } catch (SQLException erro) {
+            Logger.getLogger(connectDAO.class.getName()).log(Level.SEVERE, null, erro);
+        }
+    }
+
+    public List<String> consultaRegistroJFBD(BaseDAO obj) {
+        String tabela = obj.getTableName();
+        String campos = obj.consultaSQLValues();
+        String condicao = obj.termoSQLWhereById();
+        conn = connectDB();
+        Statement stmt;
+        ResultSet dados;
+        List<String> lista = new ArrayList<>();
+        try {
+            stmt = conn.createStatement();
+            
+            String sql;
+            sql = "SELECT "+campos+" FROM dbo."+tabela+" WHERE "+condicao;
+            JOptionPane.showMessageDialog(null, "String de select: "+sql);
+            
+            try {
+                dados = stmt.executeQuery(sql);
+                
+                if (!dados.next()) {
+                    JOptionPane.showMessageDialog(null, "Registro não encontrado!");
+                }
+
+                int columnCount = dados.getMetaData().getColumnCount();
+                for (int index = 1; index <= columnCount; index++) {
+                    lista.add(dados.getString(index));
+                }
+            } catch (SQLException erro) {
+                JOptionPane.showMessageDialog(null, "Erro de conexão, connectDAO - Mensagem => "+erro.getMessage());
+                JOptionPane.showMessageDialog(null, "\n Erro de conexão, connectDAO - Estado => "+erro.getSQLState());
+                JOptionPane.showMessageDialog(null, "\n Erro de conexão, connectDAO - Código => "+erro.getErrorCode());
+            }
+            conn.close();
+        } catch (SQLException erro) {
+            Logger.getLogger(connectDAO.class.getName()).log(Level.SEVERE, null, erro);
+        }
+        return lista;
+    }
+    
+    public ResultSet consultaTodoRegistroJFBD(BaseDAO obj) {
+        String tabela = obj.getTableName();
+        String campos = obj.consultaSQLValues();
+        conn = connectDB();
+        Statement stmt;
+        ResultSet dados;
+        try {
+            stmt = conn.createStatement();
+            
+            String sql = "SELECT "+campos+" FROM dbo."+tabela;
+            System.out.println(sql);
+            JOptionPane.showMessageDialog(null, "String de select: "+sql);
+            
+            try {
+                dados = stmt.executeQuery(sql);
+                return dados;
+                
+            } catch (SQLException erro) {
+                JOptionPane.showMessageDialog(null, "Erro de conexão, connectDAO - Mensagem => "+erro.getMessage());
+                JOptionPane.showMessageDialog(null, "\n Erro de conexão, connectDAO - Estado => "+erro.getSQLState());
+                JOptionPane.showMessageDialog(null, "\n Erro de conexão, connectDAO - Código => "+erro.getErrorCode());
+            }
+            conn.close();
+        } catch (SQLException erro) {
+            Logger.getLogger(connectDAO.class.getName()).log(Level.SEVERE, null, erro);
+        }
+        return null;
+    }
+}
+
